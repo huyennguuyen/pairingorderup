9943049646f8ab995a2052bede1454c3b4f78b77
Huyen Nguyen <hn078722@gmail.com>
2022-04-20 15:15:06 -0700

    need session page to work

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,18 @@
 from flask import Flask
 from app.config import Configuration
-from app.routes import orders
-from app.models import db
+from app.routes import orders, session
+from app.models import db, Employee
+from flask_login import LoginManager
 
 app = Flask(__name__)
 app.config.from_object(Configuration)
 app.register_blueprint(orders.bp)
+app.register_blueprint(session.bp)
 db.init_app(app)
+
+login = LoginManager(app)
+login.login_view = "session.login"
+
+@login.user_loader
+def load_user(id):
+    return Employee.query.get(int(id))
